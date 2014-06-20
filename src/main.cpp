9ec11f67cf70cadb2bb9a38c66b0fae10d1d40fd
Victor Robertson <victor.robertson.iv@gmail.com>
2014-06-20 18:54:23 -0500

    glew library queries

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,49 @@
 #include <SDL.h>
+#include <GL/glew.h>
+
+#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 800
 
 int main() {
 
-	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+	SDL_Window* window = SDL_CreateWindow(
+		"OpenGL",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
+		SDL_WINDOW_OPENGL);
+
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+
+	printf("%u\n", vertexBuffer);
+
+	SDL_Event windowEvent;
+	while (true) {
+		if (SDL_PollEvent(&windowEvent)) {
+			if (windowEvent.type == SDL_QUIT) {
+				break;
+			}
+		}
+
+		SDL_GL_SwapWindow(window);
+	}
+
 	SDL_Delay(1000);
-	SDL_Quit();
+
+	SDL_GL_DeleteContext(context);
 	
+	SDL_Quit();
 }
